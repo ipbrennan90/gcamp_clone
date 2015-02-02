1074d6d260b281adca725400ba617263be6f9748
Ian Brennan <ianbrennan@Ians-MacBook-Pro.local>
2015-02-02 16:40:07 -0700

    faq rework

--- app/models/common_question.rb
+++ app/models/common_question.rb
@@ -1,8 +1,7 @@
 class CommonQuestion
   def self.question
-    frequestions={questions: []
-    frequests.each do |k|
-      k[question:]
-      k[answer:]
-    end
+    #frequestion = [{question: ["What is gCamp?", "How do I join gCamp?", "When will gCamp be done?"] },{answer: ["gCamp is an awesome tool that is going to change your life. gCamp is your one stop shop to organize all your tasks. You'll also be able to track comments that you and others make. gCamp may eventually replace all need for paper and pens in the entire world. Well, maybe not, but it's going to be pretty cool.","As soon as it's ready for the public, you'll see a signup link in the upper right. Once that's there, just click it and fill in the form!","When will gCamp be finished?","gCamp is a work in progress. That being said, it should be fully functional in the next few weeks. Functional. Check in daily for new features and awesome functionality. It's going to blow your mind. Organization is just a click away. Amazing!"]}]
+    [{question: "what?", answer: "dinosaur"}, {question: "who?", answer: "T-rex, bitch"}, {question: "When?", answer: "mesozoic mofuckas"}]
+  end
+
 end
