69013f9c73f4d70a78ed55dd54b7b0e1e7e0c18d
Ian Brennan <ian.brennan@icloud.com>
2015-02-20 21:02:56 -0700

    reverted because its friday accidental changes saving newest gCamp version

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,7 +13,7 @@ class ProjectsController < ApplicationController
     @project=Project.new(project_params)
     if @project.save
       flash[:success] = "Project was successfully created"
-      redirect_to projects_path
+      redirect_to projects_pathc
     else
       render :new
     end
